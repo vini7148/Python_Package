4d564325fc4b86ce5fd5ca9a6ae06460d62dd615
vini7148 <vgoswami714841@yahoo.com>
2020-03-06 14:02:49 +0530

    Added exeption handeling for missing imports

--- test_1/__init__.py
+++ test_1/__init__.py
@@ -1,2 +1,8 @@
+try:
+    import datetime
+except ImportError:
+    raise ImportError("This package needs datetime to execute")
+del datetime
+
 from .file_1 import time, sum, avg
 from .file_2 import hw
